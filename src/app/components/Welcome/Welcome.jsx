897fe4405e1406411d7b01443b775ae0ee69ec2f
Giovanni Lituma <giovanninet@icloud.com>
2022-09-20 12:45:10 -0400

    remove app folder

--- src/app/components/Welcome/Welcome.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './Welcome.module.css';
-
-function Welcome() {
-  return <p className={classes.message}>Hello World</p>;
-}
-
-export default Welcome;
