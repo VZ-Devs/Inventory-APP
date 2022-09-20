c5ad6a10a9ceeffc3bdfe749043370cdb96e0f42
Giovanni Lituma <giovanninet@icloud.com>
2022-09-20 12:51:54 -0400

    Add new file and folders structure and Basic Styling

---
+++ src/pages/home/item/index.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import styles from '../home.module.css'
+
+export const Item = ({data: {title, price, description, category, image, quantity }}) => {
+
+  return (
+    <article className={styles.card}>
+      <div className={styles.media}>
+        <img src={image} alt={image} />
+        <h2 className={styles.title}>{title.substring(0, 9)}</h2>
+        <span className={styles.tag}>{category.substring(0, 9)}</span>
+        <h3 className={styles.price}>{price}</h3>
+      </div>
+      <div className={styles.content}>
+        <p className={styles.quantity}><sup>QT</sup>{quantity}</p>
+        <p className={styles.description}>{description}</p>
+      </div>
+    </article>
+  )
+} 
