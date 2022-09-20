d4638d38da1ebb66836a8af5821d85249f9c8bf2
Giovanni Lituma <giovanninet@icloud.com>
2022-09-20 12:37:00 -0400

    Add alias config option to the build tool to use the src path

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,5 @@
 import dotenv from 'dotenv';
+import * as path from 'path'
 dotenv.config();
 
 import { defineConfig } from 'vite';
@@ -16,6 +17,9 @@ export default defineConfig({
         changeOrigin: true,
       },
     },
+  },
+  resolve: {
+    alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   build: {
     outDir: 'dist/app',
